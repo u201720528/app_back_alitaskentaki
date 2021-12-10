285d891a432b279e7879d797583b63f1e23a6e87
Fernando Vinchenzo Aragón Palomino <58162684+fvchenzo@users.noreply.github.com>
2021-12-10 13:40:10 -0500

    adminpedido, detped y usuario

---
+++ AlitasKentaki.API/Controllers/AdminDetallePedidoController.cs
@@ -0,0 +1,43 @@
+﻿using DBContext;
+using DBEntity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NSwag.Annotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AlitasKentaki.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/admindetallepedido")]
+    [ApiController]
+    public class AdminDetallePedidoController : Controller
+    {
+        protected readonly IAdminDetallePedidoRepository _adminDetallePedidoRepository;
+
+        public AdminDetallePedidoController(IAdminDetallePedidoRepository adminDetallePedidoRepository)
+        {
+            _adminDetallePedidoRepository = adminDetallePedidoRepository;
+        }
+
+        [Produces("application/json")]
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("obtenerpedidoporusuariopedido")]
+        public ActionResult obtenerDetallePedidoPorPedido(int idpedido)
+        {
+            var ret = _adminDetallePedidoRepository.obtenerDetallePedidoPorPedido(idpedido);
+
+            if (ret == null)
+                return StatusCode(401);
+
+            return Json(ret);
+
+        }
+    }
+}
---
+++ AlitasKentaki.APIBusiness.DBContext/Repository/AdminDetallePedidoRepository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using DBEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Linq;
+
+namespace DBContext
+{
+    public class AdminDetallePedidoRepository : BaseRepository, IAdminDetallePedidoRepository
+    {
+        public BaseResponse obtenerDetallePedidoPorPedido(int idpedido)
+        {
+            var returnEntity = new BaseResponse();
+            var entitiesProject = new List<EntityAdminDetallePedido>();
+            var productoRepository = new ProductoRepository();
+            try
+            {
+                using (var db = GetSqlConnection())
+                {
+                    const string sql = @"usp_DetallePedido_X_Pedido";
+
+                    var p = new DynamicParameters();
+                    p.Add(name: "@IDPEDIDO", value: idpedido, dbType: DbType.Int32, direction: ParameterDirection.Input);
+
+                    entitiesProject = db.Query<EntityAdminDetallePedido>(
+                        sql: sql,
+                        param: p,
+                        commandType: CommandType.StoredProcedure
+                       ).ToList();
+
+                    if (entitiesProject.Count > 0)
+                    {
+                        foreach (var project in entitiesProject)
+                        {
+                            project.producto = (EntityProducto)productoRepository.ObtenerProductoPorId(project.idproducto).data;
+
+                        }
+
+                        returnEntity.issuccess = true;
+                        returnEntity.errorcode = "0000";
+                        returnEntity.errormessage = string.Empty;
+                        returnEntity.data = entitiesProject;
+                    }
+                    else
+                    {
+                        returnEntity.issuccess = false;
+                        returnEntity.errorcode = "0000";
+                        returnEntity.errormessage = string.Empty;
+                        returnEntity.data = null;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                returnEntity.issuccess = false;
+                returnEntity.errorcode = "0001";
+                returnEntity.errormessage = ex.Message;
+                returnEntity.data = null;
+            }
+            return returnEntity;
+        }
+    }
+}
